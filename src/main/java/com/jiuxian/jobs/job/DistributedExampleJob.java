02f10c1152c584ba7482eab5e957c2e7e07ef987
yuxuanwang <crnlmcn@gmail.com>
2016-11-04 22:50:35 +0800

    update example

--- src/main/java/com/jiuxian/jobs/job/DistributedExampleJob.java
+++ src/main/java/com/jiuxian/jobs/job/DistributedExampleJob.java
@@ -11,16 +11,24 @@ import com.jiuxian.mossrose.job.DistributedJob;
 import net.logstash.logback.argument.StructuredArguments;
 
 public class DistributedExampleJob implements DistributedJob<String> {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleExampleJob.class);
 
 	@Override
 	public List<String> slice() {
-		return Splitter.on(" ").splitToList("This is a test on the mossrose distributed job, how are you feeling?");
+		return Splitter.on(" ").splitToList("A B C D E F G H I J K L M N O P Q R S T U");
 	}
 
 	@Override
 	public void execute(String item) {
+		if ("D".equals(item)) {
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		LOGGER.info("DistributedJob: {}", StructuredArguments.value("item", item));
 	}
 
